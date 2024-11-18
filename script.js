35c19e744126f91a0cfc7a38001526f5327703ce
Davide Baroni <baronidavide@itis-molinari.eu>
2024-11-18 13:27:04 +0100

    carica

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/////////////////////////////////////////////////////FETCH////////////////////////////////////////////////////////////
 let myToken, myKey, tokenMap;
 fetch('./conf.json') // carica le variabili da conf.json
   .then((response) => {
@@ -15,6 +16,30 @@ fetch('./conf.json') // carica le variabili da conf.json
     console.log(tokenMap);
   })
   .catch((error) => console.error('Errore:', error));
+
+
+
+function carica() {
+  return fetch('https://ws.cipiaceinfo.it/cache/get', {
+          headers: {
+              'Content-Type': 'application/json',
+              key: myToken,
+          },
+          method: 'POST',
+          body: JSON.stringify({
+              key: myKey,
+          }),
+      })
+      .then((r) => r.json())
+      .then((r) => {
+          console.log('Dati caricati:', r.result);
+          diz = r.result || {};
+          
+      })
+      .catch((err) => console.log('Errore durante il caricamento:', err));
+}
+
+
 
 /////////////////////////////////////////////////////////////////////////////////////MODALE////////////////////////////////////////////////////////
 const modalContainer = document.getElementById('modal-container');
